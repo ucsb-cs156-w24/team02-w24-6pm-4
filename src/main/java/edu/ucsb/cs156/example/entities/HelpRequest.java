20008b7afe7127165b7be3676e3a95a308f8e339
ryanvo504 <ryanvo@ucsb.edu>
2024-02-10 14:48:27 -0800

    create controller for help request

--- src/main/java/edu/ucsb/cs156/example/entities/HelpRequest.java
+++ src/main/java/edu/ucsb/cs156/example/entities/HelpRequest.java
@@ -16,8 +16,8 @@ import lombok.Builder;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity(name = "helprequests")
-public class HelpRequests {
+@Entity(name = "helprequest")
+public class HelpRequest {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
--- src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
@@ -1,11 +1,11 @@
 package edu.ucsb.cs156.example.repositories;
 
-import edu.ucsb.cs156.example.entities.HelpRequests;
+import edu.ucsb.cs156.example.entities.HelpRequest;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface HelpRequestsRepository extends CrudRepository<HelpRequests, Long> {
+public interface HelpRequestRepository extends CrudRepository<HelpRequest, Long> {
 }
