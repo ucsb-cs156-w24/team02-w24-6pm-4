12132407c352668cace1c748371810f09ec82378
ryanvo504 <ryanvo@ucsb.edu>
2024-02-14 00:52:47 -0800

    hm

--- src/main/java/edu/ucsb/cs156/example/controllers/HelpRequestsController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/HelpRequestsController.java
@@ -54,7 +54,7 @@ public class HelpRequestsController extends ApiController {
             @Parameter(name="tableOrBreakoutRoom") @RequestParam String tableOrBreakoutRoom,
             @Parameter(name="explanation") @RequestParam String explanation,
             @Parameter(name="solved") @RequestParam Boolean solved,
-            @Parameter(name="requestTime (in iso format, e.g. YYYY-mm-ddTHH:MM:SS; see https://en.wikipedia.org/wiki/ISO_8601)") @RequestParam("requestTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime requestTime)
+            @Parameter(name="requestTime") @RequestParam("requestTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime requestTime)
             throws JsonProcessingException {
 
         // For an explanation of @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
